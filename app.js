f18fc433195957f9cb7580c44ae8179a33ecfcfe
leadenhawk <leadenhawk@tutanota.de>
2017-08-24 13:29:43 +1200

    setup socket.io

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// Setup express app
+var app = express();
+var server = app.listen(4000, function(){
+  console.log('listening to port 4000...');
+});
+
+// Static files
+app.use(express.static('public')); //serves the files located in the public folder
+
+// Socket Setup
+var io = socket(server);
+
+io.on('connection', function(){
+  console.log("made socket connection",socket.id);
+});
---
+++ public/socket.js
@@ -0,0 +1,2 @@
+//make connection to front-end
+var socket = io.connect('http://192.168.1.70:4000');
