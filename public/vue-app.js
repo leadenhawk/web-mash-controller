de98a73047437bd49ef0322286c3e72a5dc82bbd
leadenhawk <leadenhawk@tutanota.de>
2017-08-26 16:10:08 +1200

    set up two very basic vue instances

---
+++ public/vue-app.js
@@ -0,0 +1,25 @@
+var title = new Vue({
+  el: '#vue-app-title',
+  data: {
+    name: "Nathan's Vue based Socket.io & johnny-five mash controller"
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+});
+
+var temperature = new Vue({
+  el: '#vue-app-temperature',
+  data: {
+    temp: "temp"
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+});
